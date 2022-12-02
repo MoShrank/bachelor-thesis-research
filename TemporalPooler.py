3bd7b742c425b7fe38bd4a371927e23154a3ab73
Moritz Eich <hey@moritz.dev>
2022-12-02 10:14:26 +0100

    add: temporal pooler and run experiment setup

---
+++ TemporalPooler.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class TemporalPooler:
+    def __init__(self, no_cells: int, no_columns: int):
+
+        self.no_cells = no_cells
+        self.no_columns = no_columns
+
+        # connections to other cells in the same column
+
+        # permanences of connections to other cells in the same column
+
+        # state of cells
+
+        # time step
+
+    def compute(self, winning_columns: np.ndarray):
+        pass
