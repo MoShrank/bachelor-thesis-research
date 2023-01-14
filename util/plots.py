3745c1963b00efae5f270fb1ef4a032962ba74a9
Moritz Eich <hey@moritz.dev>
2023-01-14 19:51:45 +0100

    add: grid cell stuff

--- util/plots.py
+++ util/plots.py
@@ -35,13 +35,31 @@ def plot_images(arr: np.ndarray):
     plt.show()
 
 
-def scatter_plot(xs: List, ys: List):
+def scatter_plot(xs: List, ys: List, lim=(0, 28), plot_arrows: bool = False):
     # draw quadartic scatter plot with xlim and ylim and grid with grid lines per 1 unit
     plt.figure(figsize=(7, 7))
     plt.scatter(xs, ys)
-    plt.xlim(0, 28)
-    plt.ylim(0, 28)
+    plt.xlim(lim[0], lim[1])
+    plt.ylim(lim[0], lim[1])
     plt.grid(b=True, which="major", color="#666666", linestyle="-")
     plt.minorticks_on()
     plt.grid(b=True, which="minor", color="#999999", linestyle="-", alpha=0.2)
+
+    if plot_arrows:
+
+        for i in range(len(xs) - 1):
+            plt.arrow(
+                xs[i],
+                ys[i],
+                xs[i + 1] - xs[i],
+                ys[i + 1] - ys[i],
+                head_width=0.3,
+                head_length=0.3,
+                length_includes_head=True,
+                fc="k",
+                ec="k",
+            )
+
+        plt.scatter(xs[0], ys[0], c="r")
+
     plt.show()
